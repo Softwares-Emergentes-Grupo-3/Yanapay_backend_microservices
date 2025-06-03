a96c031b8c4e603f6498c26faf7817e0ad70d523
Diego Defilippi <defsan35@gmail.com>
2025-06-03 00:12:35 -0500

    feat: Add backend structure

---
+++ yanapay/src/main/java/agronova/yanapay/YanapayApplication.java
@@ -0,0 +1,13 @@
+package agronova.yanapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanapayApplication.class, args);
+	}
+
+}
---
+++ yanapay/src/test/java/agronova/yanapay/YanapayApplicationTests.java
@@ -0,0 +1,13 @@
+package agronova.yanapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
